b3aed8c5c9a0068442cffb8f2ea63a462e546af0
oiat11 <fu.qia@northeastern.edu>
2025-05-25 18:32:55 -0700

    connect to the database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,22 +1,27 @@
-import dontev from 'dotenv';
+import dotenv from 'dotenv'; // Fixed typo: was 'dontev'
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 
+dotenv.config();
+
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI as string).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-}
-);
-
+});
 
