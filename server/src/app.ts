2c54273e093a80c43e98c4d4de9bedd5ad7d25d1
oiat11 <fu.qia@northeastern.edu>
2025-05-26 09:11:53 -0700

    set up passport and use google oauth for user authentication

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,8 +20,7 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
-
